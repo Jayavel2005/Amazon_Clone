6c6a01ab150e34b10c8808f92cc611b10685d04b
Jayavel <jayavel1520@gmail.com>
2024-11-23 22:27:41 +0530

    Making cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -60,6 +60,7 @@ products.forEach((product) => {
     
 });
 productsGrid.innerHTML = productsHTML;
+
 document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
     button.addEventListener("click", ()=>{
         const productId = button.dataset.productId;
@@ -86,11 +87,16 @@ document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
             );
         }
         
-        console.log(cart);
+        let cartQuantity = 0;
+        cart.forEach((item)=>{
+            cartQuantity += item.quantity;
+        })
+        document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
         
         
         
         
-    })
-})
+        
+    });
+});
 
