238fefc4e2bfa7134037e8ac4482e22a3e427d2b
Jayavel <jayavel1520@gmail.com>
2024-11-24 16:05:05 +0530

    Remove cart is now interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem)=>{
     
     
     cartItems +=`
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingItem.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -96,7 +96,11 @@ document.querySelectorAll(".js-delete-quantity-link").forEach((link)=>{
     link.addEventListener("click",()=>{
         const productId = link.dataset.productId;
         removeFromCart(productId);
-        console.log(cart);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`)
+        container.remove();
+        
+        
         
     })
 })
