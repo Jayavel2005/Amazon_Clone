20f03d2ff9f63a86c031219ef1f4357db5ed1003
Jayavel <jayavel1520@gmail.com>
2025-04-09 15:36:45 +0530

    Add product data and cart functionality with dynamic cart updates

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -1,4 +1,42 @@
-import {products} from './products.js';
+const products = [
+  {
+    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    price: 100
+  },
+  {
+    image: 'images/products/intermediate-composite-basketball.jpg',
+    name: 'Intermediate Size Basketball',
+    rating: {
+      stars: 4,
+      count: 127,
+    },
+    price: 1095,
+  },
+  {
+    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+    rating: {
+      stars: 4.5,
+      count: 56,
+    },
+    price: 199,
+  }
+  ,
+  {
+    image: 'images/products/black-2-slot-toaster.jpg',
+    name: '2 Slot Toaster - Black',
+    rating: {
+      stars: 5,
+      count: 2197,
+    },
+    price: 1999,
+  }
+];
 
 
 
@@ -50,7 +88,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}" data-product-image="${product.image}" data-product-price="${product.price}">
             Add to Cart
           </button>
         </div>`;
@@ -59,3 +97,36 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productHtml;
+
+
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+
+    const productName = button.dataset.productName;
+
+    let matchingItem;
+    cart.forEach((item) => {
+      if (item.productName === productName) {
+        matchingItem = item;
+      }
+    })
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push(
+        {
+          productName: productName,
+          quantity: 1,
+        }
+      )
+    }
+
+    console.log(cart);
+
+
+
+
+  })
+})
